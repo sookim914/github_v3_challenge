6320602c11b2af4bbc50a9d555ca6cb657a927bf
sookim-Boston <soojeong914@gmail.com>
2020-02-12 03:54:04 -0500

    Add event handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+// require event file
+const buttons = require('./buttons/events')
+
+$(() => {
+  // when repo button is clicked, call onGetRepos
+  $('#hot_repo').on('click', buttons.onGetRepos)
+  // when user button is clicked, call onGet
+  $('#prolific_users').on('click', buttons.onGetUsers)
+})
